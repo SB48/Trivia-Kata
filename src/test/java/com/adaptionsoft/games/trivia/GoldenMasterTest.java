25eb4d7889d3f0267c360433c5ae9a5c558d8a0a
sb48 <sophiebiber@icloud.com>
2021-03-24 14:16:54 +0000

    Added golden master test

--- src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/GoldenMasterTest.java
@@ -1,25 +1,27 @@
 package com.adaptionsoft.games.trivia;
 
+import static org.approvaltests.Approvals.verify;
 import static org.junit.Assert.*;
 
 import com.adaptionsoft.games.uglytrivia.Game;
 import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.Random;
 
 public class GoldenMasterTest {
-	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final ByteArrayOutputStream OUT_CONTENT = new ByteArrayOutputStream();
+	private final int SEED = 100;
 
 	//Use a random seed
-	@Test
 	public void gameRuns() throws Exception {
 		boolean notAWinner;
 		Game game = new Game();
 		game.add("Chet");
 		game.add("Pat");
 		game.add("Sue");
-		Random rand = new Random();
+		Random rand = new Random(SEED);
 		do {
 			game.roll(rand.nextInt(5) + 1);
 			if (rand.nextInt(9) == 7) {
@@ -31,6 +33,13 @@ public class GoldenMasterTest {
 		} while (notAWinner);
 	}
 
+	@Test
+	public void game_output() throws Exception {
+		System.setOut(new PrintStream(OUT_CONTENT));
+		gameRuns();
+		verify(OUT_CONTENT.toString().trim());
+
+	}
 
 	//This test currently breaks the game as there can only be 6 players but you can
 	// keep adding without an exception thrown
@@ -46,21 +55,6 @@ public class GoldenMasterTest {
 		assertEquals("Only 5 players allowed", exception.getMessage());
 	}
 
-//	@Test
-//	public void check_current_player_index(){
-//		Game game = new Game();
-//		String expectedOutput = outContent.toString();
-//
-//		assertEquals(outContent.toString()), (
-//				"Paul was added\n" +
-//						"They are player number 1\n" +
-//						"Paul is the current player\n" +
-//						"They have rolled a 7\n" +
-//						"Paul's new location is 7\n" +
-//						"The category is Rock\n" +
-//						"Rock Question 0\n");
-//		)
-//
-//	}
+
 
 }
